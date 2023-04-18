891a2357ff62b139dfa1bd140f38a4923496bcf7
paddy <pdd337816464@163.com>
2023-04-18 14:25:17 +0800

    增加 - Layout的布局

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,40 +1,70 @@
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout as AntdLayout, Menu, theme } from 'antd';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout as AntdLayout, Menu, MenuProps, Dropdown, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/router';
 import React from 'react';
 import Image from 'next/image'
 import styles from './index.module.css';
 
+
+
 const { Header, Content, Sider } = AntdLayout;
 
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
+const ITEMS = [
+  {
+    label: "图书管理",
+    key: "book",
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
+    children: [
+      { label: "图书列表", key: "/book" },
+      { label: "图书添加", key: "/book/add" }, 
+    ]
   },
-);
+  {
+    label: "借阅管理",
+    key: "borrow",
+
+    children: [
+      { label: "借阅列表", key: "/borrow" },
+      { label: "借阅添加", key: "/borrow/add" }, 
+    ]
+  },
+  {
+    label: "分类管理",
+    key: "category",
+
+    children: [
+      { label: "分类列表", key: "/category" },
+      { label: "分类添加", key: "/category/add" }, 
+    ]
+  },
+  {
+    label: "用户管理",
+    key: "user",
+
+    children: [
+      { label: "用户列表", key: "/user" },
+      { label: "用户添加", key: "/user/add" }, 
+    ]
+  }
+];
+
+const USER_ITEMS = [
+  {
+    label: '用户中心',
+    key: '1',
+  },
+  {
+    label: '登出',
+    key: '2',
+  },
+];
 
 export function Layout({ children }) {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const router = useRouter();
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    router.push(e.key);
+  };
 
   return (
     <AntdLayout>
@@ -42,16 +72,28 @@ export function Layout({ children }) {
         <div className={styles.logo}>
           <Image src="/logo.svg" width={30} height={30} alt='logo' />
           <span>图书馆管理系统</span>
+          <span className={styles.user}>
+            <Dropdown menu={{ items: USER_ITEMS }}>
+              <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    用户名
+                    <DownOutlined />
+                  </Space>
+              </a>
+            </Dropdown>
+          </span>
+      
         </div>
       </Header>
       <AntdLayout>
-        <Sider width={200} style={{ background: colorBgContainer }}>
+        <Sider width={200}>
           <Menu
             mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
+            defaultSelectedKeys={['book']}
+            defaultOpenKeys={['book']}
             style={{ height: '100%', borderRight: 0 }}
-            items={items2}
+            items={ITEMS}
+            onClick={handleMenuClick}
           />
         </Sider>
         <AntdLayout style={{ padding: '0 24px 24px' }}>
@@ -65,7 +107,6 @@ export function Layout({ children }) {
               padding: 24,
               margin: 0,
               minHeight: 280,
-              background: colorBgContainer,
             }}
           >
             Content
